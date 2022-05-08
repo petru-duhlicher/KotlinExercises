92ff4fff6b41bb91a9c0cf7fa961338ecc552489
petru-duhlicher <104087088+petru-duhlicher@users.noreply.github.com>
2022-05-08 23:42:48 +0300

    Count up and down  (#7)
    * problem 7
    
    * problem 7

---
+++ src/main/kotlin/ExerciseNr7.kt
@@ -0,0 +1,11 @@
+/*Given positive integer n, implement a function that returns a list containing all steps up from 0 to n and down to 0.
+Examples:
+countUpAndDown(1) // [0, 1, 0]
+
+countUpAndDown(2) // [0, 1, 2, 1, 0]
+*/
+
+fun main(args: Array<String>){
+    println(countUpAndDown(2))
+}
+fun countUpAndDown(n: Int): List<Int> = (0 until n) + (n downTo 0)
