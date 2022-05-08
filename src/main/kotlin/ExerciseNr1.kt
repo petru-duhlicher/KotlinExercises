db5f9f89081dfa23dd1a136295767fb907ae63ad
petru-duhlicher <104087088+petru-duhlicher@users.noreply.github.com>
2022-05-08 23:44:22 +0300

    Capitalize the first letter of every word. (#1)
    * capitalizes the first letter of every word
    
    * modified function ExerciseNr1
    
    * modified exercises 6, 7, 10, 11
    
    * problem nr.1
    
    * problem nr.1

---
+++ src/main/kotlin/ExerciseNr1.kt
@@ -0,0 +1,30 @@
+//Let the fun begin!!!
+
+// Exercise Nr.1
+/*Given a string, implement a function that capitalizes the first letter of every word in that string.ad
+Examples:
+capitalizeSentence("flower") // Flower
+
+capitalizeSentence("this is a house") // This Is A House
+*/
+
+
+ //uncomment following code for a simple run of the function
+//fun main(args: Array<String>){
+//
+//    println(capitalizeSentence("this is a house"))
+//}
+
+// run this
+
+// uncomment to run the
+
+//fun String.capitalizeWords(): String = split(" ").map { it.capitalize() }.joinToString(" ")
+//var s: String = "this is a house"
+
+
+fun capitalizeSentence(str: String?): String? {
+     return if (str == null || str.isEmpty()) {
+        str
+    } else  return str.substring(0, 1).uppercase() + str.substring(1)
+}
