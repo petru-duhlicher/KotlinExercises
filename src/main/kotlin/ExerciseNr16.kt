1384d0b89a9d90b7ca61cb4205ccf8191538528f
petru-duhlicher <petru.duhlicher@crunchyroll.com>
2022-05-09 09:55:15 +0300

    problem 16 of solution

--- src/main/kotlin/ExerciseNr16.kt
+++ src/main/kotlin/ExerciseNr16.kt
@@ -18,16 +18,16 @@ decapitalizeConst("__FOO_BAR_BAZ") // fooBarBaz
 //
 
 
-//fun decapitalizeConst(str: String): String? {
-//    val words = str.split("_").filter { it.isNotEmpty() }
-//
-//    if (words.size <= 1) return null
-//
-//    return words.mapIndexed { index, word ->
-//        if (index == 0) {
-//            word.lowercase()
-//        } else {
-//            word.first().uppercase() + word.drop(1).lowercase()
-//        }
-//    }.joinToString("")
-//}
+fun decapitalizeConst(str: String): String? {
+    val words = str.split("_").filter { it.isNotEmpty() }
+
+    if (words.size <= 1) return null
+
+    return words.mapIndexed { index, word ->
+        if (index == 0) {
+            word.lowercase()
+        } else {
+            word.first().uppercase() + word.drop(1).lowercase()
+        }
+    }.joinToString("")
+}
